fc4238572b03ce9a7d2ea0d873218afba43d2b25
kkkkkom <55373469+kkkkkom@users.noreply.github.com>
2025-02-09 23:17:04 -0800

    Update test_dist.sh

--- tools/test_dist.sh
+++ tools/test_dist.sh
@@ -4,4 +4,4 @@ export PYTHONPATH=./
 GPUS=1
 workdir=./work_dirs/svg/svg_pointT/baseline_nclsw_grelu
 OMP_NUM_THREADS=$GPUS torchrun --nproc_per_node=$GPUS --master_port=$((RANDOM + 10000)) tools/test.py \
-	 $workdir/svg_pointT.yaml  $workdir/best.pth --dist
+	 $workdir/svg_pointT.yaml  $workdir/latest.pth --dist
